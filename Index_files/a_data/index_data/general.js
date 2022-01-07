5e46ba4213938a74993da541f970d1ee1ca60bc7
Nightshade <38169527+n1ghtshade@users.noreply.github.com>
2022-01-06 22:05:00 -0500

    Index

---
+++ Index_files/a_data/index_data/general.js
@@ -0,0 +1,58 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+	  $(document).ready(function($) {
+	    $("#twitter").tweet({
+	      avatar_size: 0,
+	      count: 1,
+	      username: "theplayforge",
+	      loading_text: "loading tweets..."
+	    });   
+	});
+	
+	
+	$(document).ready(function() {
+
+		$("a.lightbox").fancybox({
+			'titleShow'		: false,
+			'transitionIn'	: 'elastic',
+			'transitionOut'	: 'elastic'
+		});
+	
+	});
+	
+
+
+}
+/*
+     FILE ARCHIVED ON 16:38:03 Feb 05, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:57:53 Jan 07, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 278.486
+  exclusion.robots: 201.653
+  exclusion.robots.policy: 201.638
+  xauthn.identify: 117.943
+  xauthn.chkprivs: 83.459
+  RedisCDXSource: 0.865
+  esindex: 0.009
+  LoadShardBlock: 44.356 (3)
+  PetaboxLoader3.datanode: 96.189 (4)
+  CDXLines.iter: 25.698 (3)
+  load_resource: 1946.414
+  PetaboxLoader3.resolve: 1888.568
+*/
